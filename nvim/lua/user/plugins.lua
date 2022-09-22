473655063f77a8c9911fdc2feccdd880509432e1
Zack Baker <zack.baker@Mac0552Pro.local>
2022-09-22 14:56:54 -0600

    Addig config to indent-line

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -118,7 +118,10 @@ return packer.startup(function(use)
   use {
       "lukas-reineke/indent-blankline.nvim",
       config = function ()
-        require("indent_blankline").setup {}
+        require("indent_blankline").setup {
+            show_current_context = true,
+            show_current_context_start = true,
+        }
       end
   }
 
