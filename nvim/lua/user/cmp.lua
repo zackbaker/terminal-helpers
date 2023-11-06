fd9f819a334eb6c11e0e76f66b6e180c557f19f1
Zack Baker <zbakerdesign@gmail.com>
2023-11-05 18:15:03 -0700

    Deleting old code from cmp.lua

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -1,68 +1,9 @@
--- local cmp_status_ok, cmp = pcall(require, "cmp")
--- if not cmp_status_ok then
---   return
--- end
---
--- local snip_status_ok, luasnip = pcall(require, "luasnip")
--- if not snip_status_ok then
---   return
--- end
---
--- local ap_status_ok, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
--- if not ap_status_ok then
---     return
--- end
--- cmp.event:on(
---   'confirm_done',
---   cmp_autopairs.on_confirm_done()
--- )
---
--- require("luasnip/loaders/from_vscode").lazy_load()
---
--- local check_backspace = function()
---   local col = vim.fn.col "." - 1
---   return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
--- end
-
---   פּ ﯟ   some other good icons
--- local kind_icons = {
---   Text = "",
---   Method = "m",
---   Function = "",
---   Constructor = "",
---   Field = "",
---   Variable = "",
---   Class = "",
---   Interface = "",
---   Module = "",
---   Property = "",
---   Unit = "",
---   Value = "",
---   Enum = "",
---   Keyword = "",
---   Snippet = "",
---   Color = "",
---   File = "",
---   Reference = "",
---   Folder = "",
---   EnumMember = "",
---   Constant = "",
---   Struct = "",
---   Event = "",
---   Operator = "",
---   TypeParameter = "",
--- }
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup {
-  -- snippet = {
-  --   expand = function(args)
-  --     luasnip.lsp_expand(args.body) -- For `luasnip` users.
-  --   end,
-  -- },
   mapping = {
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
@@ -94,27 +35,5 @@ cmp.setup {
     }),
     ['<cr>'] = cmp.mapping.confirm({ select = false }),
   },
-  -- formatting = {
-  --   fields = { "kind", "abbr", "menu" },
-  --   format = function(entry, vim_item)
-  --     -- Kind icons
-  --     vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-  --     vim_item.menu = ({
-  --       pyright = "(PyRight)",
-  --       nvim_lsp = "(LSP)",
-  --       nvim_lua = "(LUA)",
-  --       buffer = "(Buffer)",
-  --       path = "(Path)",
-  --     })[entry.source.name]
-  --     return vim_item
-  --   end,
-  -- },
-  -- sources = {
-  --   { name = "pyright" },
-  --   { name = "nvim_lsp" },
-  --   { name = "nvim_lua" },
-  --   { name = "buffer" },
-  --   { name = "path" },
-  -- },
 }
 
