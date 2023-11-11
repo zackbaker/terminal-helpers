35f4a1d0f82349716f1741692b95c9bf0c66a8a6
Zack Baker <zbakerdesign@gmail.com>
2023-11-10 18:42:14 -0700

    Installing packages for pyenv

--- setup/python_setup.py
+++ setup/python_setup.py
@@ -4,6 +4,26 @@ from . import helpers
 def run():
     home_dir = helpers.get_homedir()
     if not os.path.isdir(f'{home_dir}/.pyenv'):
+        install_packages = [
+            'build-essential',
+            'curl',
+            'libbz2-dev',
+            'libffi-dev',
+            'liblzma-dev',
+            'libncursesw5-dev',
+            'libreadline-dev',
+            'libsqlite3-dev',
+            'libssl-dev',
+            'libxml2-dev',
+            'libxmlsec1-dev',
+            'llvm',
+            'make',
+            'tk-dev',
+            'wget',
+            'xz-utils',
+            'zlib1g-dev'
+        ]
+        helpers.run_cmd(f'sudo apt-get install {" ".join(install_packages)}')
         helpers.run_cmd(f'git clone https://github.com/pyenv/pyenv.git {home_dir}/.pyenv')
         helpers.run_cmd(f'cd {home_dir}/.pyenv && src/configure && make -C src')
         helpers.run_cmd(f'git clone https://github.com/pyenv/pyenv-virtualenv.git {home_dir}/.pyenv/plugins/pyenv-virtual')
