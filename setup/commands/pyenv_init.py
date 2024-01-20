9c0cca3b09574b3cecd74fbfc0b4f00b36001837
Zack Baker <zbakerdesign@gmail.com>
2024-01-20 15:49:58 -0700

    Adding helper for setting up pyenv virtual environments

---
+++ setup/commands/pyenv_init.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import os
+
+py_version = input('What Python version would you like this env? (ex. 3.10): ')
+os.system(f'pyenv install {py_version} -v')
+
+project_name = os.getcwd()
+project_name = '-'.join(project_name.split('/')[-2:])
+
+os.system(f'pyenv virtualenv {py_version} {project_name}-{py_version}')
+
+with open('.python-version', 'w') as fp:
+    fp.write(f'{py_version}/envs/{project_name}-{py_version}')
+
--- setup/link_commands.py
+++ setup/link_commands.py
@@ -9,7 +9,12 @@ def copy_command(command_name):
     helpers.run_cmd(f'sudo chmod 755 {command_path}/{command_name}')
 
 def run():
+    commands = [
+        'pyenv_init'
+    ]
     command_path = f'/usr/local/bin'
-    commands_dir = f'{os.getcwd()}/commands'
-    # helpers.run_cmd(f'sudo cp {commands_dir}/gopyvenv {command_path}')
+    commands_dir = f'{os.getcwd()}/setup/commands'
+
+    for command in commands:
+        helpers.run_cmd(f'sudo ln -sf {commands_dir}/{command}.py {command_path}/{command}')
 
