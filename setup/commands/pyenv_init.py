7be9b8f52a7ba0658a171933afdb43ba96334d86
Zack Baker <zbakerdesign@gmail.com>
2024-01-21 09:19:59 -0700

    Creating pyright file

--- setup/commands/pyenv_init.py
+++ setup/commands/pyenv_init.py
@@ -13,3 +13,7 @@ os.system(f'pyenv virtualenv {py_version} {project_name}-{py_version}')
 with open('.python-version', 'w') as fp:
     fp.write(f'{py_version}/envs/{project_name}-{py_version}')
 
+with open('pyrightconfig.json', 'w') as fp:
+    home_dir = os.path.expanduser('~')
+    fp.write('{"venvPath":' + f' "{home_dir}/.pyenv/versions/", "venv": "{project_name}-{py_version}"' + '}')
+
