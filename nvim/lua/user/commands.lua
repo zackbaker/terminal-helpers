9585ecce7be12e649ea316acd455c15159ce07ab
Zack Baker <zackbaker@acima-mac1177pro.lan>
2025-03-22 10:26:10 -0600

    Update to neovim formatter

--- nvim/lua/user/commands.lua
+++ nvim/lua/user/commands.lua
@@ -1,2 +1,2 @@
-vim.api.nvim_create_user_command('Format', 'lua vim.lsp.buf.formatting()', {})
+vim.api.nvim_create_user_command('Format', 'lua vim.lsp.buf.formatting({async = true})', {})
 vim.api.nvim_create_user_command('Blame', ':Gitsigns toggle_current_line_blame', {})
