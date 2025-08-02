55cd6cdea0b5cb1f18d428de04b290b8b7dd4dc2
Zack Baker <zbakerdesign@gmail.com>
2025-08-01 20:27:28 -0600

    Updating what languages the editor cares about since quite a few are broken

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = "all",
+    ensure_installed = {"python", "lua", "sql"},
     highlight = { enable = true },
     indent = { enable = true }
 }
