a9122d9e74bc767f47363fc989e7ace3e2a4a46c
Zack Baker <zbakerdesign@gmail.com>
2026-04-24 19:51:09 -0600

    Fixing treesitter to handle the new versioning with the main branch instead of the master branch where configs is changed to config

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,19 +1,18 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system {
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        install_path,
-    }
-    print "Installing packer close and reopen Neovim..."
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
     vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd [[
@@ -82,7 +81,7 @@ return packer.startup(function(use)
 
     -- Treesitter
     use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate", }
-    use {"nvim-treesitter/playground"}
+    -- use {"nvim-treesitter/playground"}
 
     -- Comments Just starting it here, if I want to change settings I'll move into it's own file
     use {
--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require'nvim-treesitter.config'.setup {
     ensure_installed = {"python", "lua", "sql", "query"},
     highlight = { enable = true },
     indent = { enable = true }
